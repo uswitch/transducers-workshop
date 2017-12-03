afd661a79da9b8c6b8fb1f682143c2495e7ecc1c
reborg <reborg@reborg.net>
2017-12-03 05:23:42 +0000

    lab01 updates

--- src/transducers_workshop/lab01.clj
+++ src/transducers_workshop/lab01.clj
@@ -8,7 +8,11 @@
   []
   (edn/read-string (slurp "feed.edn")))
 
-; Data in the feed are stored as group of keys:
+; Data in the feed are stored as the following group of keys. The
+; :product key contains many other keys that are not displayed here
+; for brevity. Our goal is to change the shape of this input map and be
+; able to filter only the products we are interested. Have a look at
+; the shape of the incoming data below:
 
 ; INPUT shape
 ; [{:fee-attributes []
@@ -18,8 +22,12 @@
 ;   :product {}
 ;   :created-at 111111}]
 
-; Take a look at the actual feed in ./feed.edn.
-; We want to merge everything into the :product key
+; If you want, also take a look at the actual feed in ./feed.edn.
+; You'll see all the keys inside a product.
+; We want to merge everything into the :product key, so after a few
+; transformations, this is the expected shape of the output list.
+; key1, key2 etc, are the keys previously contained inside the :product map
+; that we are getting rid of:
 
 ; OUTPUT shape:
 ; [{:key1 "val1"
@@ -33,40 +41,55 @@
 
 ; Task 1: prepare the data.
 ; Write a combination of transducers to perform the following:
-; * Merge :fee-attributes and :create-at into the :product map.
-; * Transform :created-at date from long into java.util.Date
-; Place the transducers into the "xform" function.
+; 1 Merge :fee-attributes and :create-at into the :product map.
+; 2 Transform :created-at date from long into java.util.Date
+; Place the transducers into the "xform" function. Test it using the
+; "products" function.
 
-(defn xform [params]
+(def xform
   ;; ... compose transducers here
-  )
+  (comp))
+
+;; tests your progress with:
+(defn products [feed]
+  (eduction xform feed))
+
+; Is the map in the expected shape?
+(first (products (load-data)))
 
 ; Task 2: now add filtering to perform the following:
-; * Only show a product if it is :visible and :online (they should be true)
-; * If the search params contain a :company-id, then filter for that company ID
-; * If the search params contain a :repayment-method type, then filter pruducts with that repayment type set to 'true'
-; * If the search params contain a :loan-amount, only show products :min-loan-amount <= loan-amout <= :max-loan-amount
-; Can you guess why using eduction here? Why not sequence or transduce?
+; 1. Only show a product if it is :visible and :online (they should be true to be present in the list)
+; 2. If the search params contain a :company-id, then filter for that company ID
+; 3. If the search params contain a :repayment-method key (there are a few values possible),
+;    then filter pruducts with that repayment type only.
+; 4. If the search params contain a :loan-amount, only show products where
+;    :min-loan-amount <= loan-amout <= :max-loan-amount
 
+; Bonus: can you answer why wea are using eduction here? Why not sequence or transduce?
+
+(defn xform [params]
+  ;; ... add your filters to the comp.
+  ;; They depend on the content of params.
+  (comp))
+
+;; tests your progress with:
 (defn products [params feed]
   (eduction (xform params) feed))
 
-;; How to use at the REPL to veryfy if everything is working:
-; (require '[transducers-workshop.lab01 :as lab01] :reload)
+(def example-params
+  {:repayment-method :payment-method-part-repayment
+   :loan-amount 1500000})
 
-; (def xs
-;   (lab01/products {; :company-id 46
-;                    :repayment-method :payment-method-part-repayment
-;                    :loan-amount 1500000}
-;                   (lab01/load-data)))
-; (count xs)
-; 69
+(def xs (products example-params (load-data)))
+(count xs)
+; You should see 69 products with those filters.
 
 ; Task 3: store searches for company-id 46 and company-id 50.
-; sequence attaches the transducers chain to a specific collection (our feed)
-; and produces results on demand. The search can be stored (as far as the feed
-; doesn't change) and re-used in multiple places. The transducer chain won't execute
-; again and evaluated results cached.
+; Next task is about creating defs for two very frequent searches
+; (say that we notice 80% of our traffic comes from the same parameters
+; pattern). Assuming we can accept to have them in memory all the time,
+; this is boosting 80% of our traffic. The function "create-search"
+; creates those searches. What would you use, eduction or sequence? Why?
 
 (defn create-search [params]
   ;; ... create sequence here.
@@ -75,14 +98,12 @@
 (def company1 (create-search {:company-id 46}))
 (def company2 (create-search {:company-id 50}))
 
-; What is sequence allowing in this scenario? Here's an example of what you should see:
-; (map :name lab01/company1)
+; You should see something like the following:
+(map :name company1)
 ; ("Green Professional Credit Intermediary 1.5% Buyer AA123 3 0.75% Legals"
 ;  "AAA132 IO A130 BBB124 AAA125 Older Self / only) Year"
 ;  "(DMS) Clear128 Fixed Intrinsic Part/Part Switcher AA130 BiR Tracker Reward")
-; (map :name lab01/company2)
+(map :name company2)
 ; ("Loan Monthly AAA124 C/A Product A126 1% Tier 10 Starter")
 
-; What happens if company-id 50 launches a new product in the feed and I run
-; (map :name lab01/company2)
-; again?
+; Bonus: what should I do if company-id 50 launches a new product in the feed?
